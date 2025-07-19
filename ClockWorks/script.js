305587d39e7aab76bb79b29a6ced879425a3b3cd
PinJinx <rohithkpradeep@gmail.com>
2025-07-19 18:23:55 +0530

    changed loc

--- ClockWorks/script.js
+++ ClockWorks/script.js
@@ -194,7 +194,7 @@ function displayScene(sceneKey, imageToLoad = null) {
 
     if (finalImageToLoad) {
         const imgElement = document.createElement('img');
-        imgElement.src = `/Stall-game-2025/ClockWorks/assets/${finalImageToLoad}`;
+        imgElement.src = `./assets/${finalImageToLoad}`;
         imgElement.alt = `Scene: ${sceneKey}`;
         imgElement.classList.add('pixel-image');
         imgElement.onerror = () => {
